f6e144aedec64622841a20970d8f4d0171064390
J <jkom2408@gmail.com>
2020-11-09 20:09:23 -0800

    Add comments to test

--- src/tuple/test_tuple.py
+++ src/tuple/test_tuple.py
@@ -9,6 +9,7 @@ class TestCalc(unittest.TestCase):
     def tearDown(self):
         pass
 
+    # A tuple with w=1.0 is a point
     def test_is_point(self):
         tuple = Tuple(4.3, -4.2, 3.1, 1.0)
         self.assertEqual(tuple.x, 4.3)
@@ -18,6 +19,7 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(tuple.isPoint(), False)
         self.assertEqual(tuple.isVector(), True)
 
+    # A tuple with w=0 is a vector
     def test_is_vector(self):
         tuple = Tuple(4.3, -4.2, 3.1, 0.0)
         self.assertEqual(tuple.x, 4.3)
@@ -27,10 +29,12 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(tuple.isPoint(), True)
         self.assertEqual(tuple.isVector(), False)
 
+    # point() creates tuples with w=1
     def test_point_creates_tuple_wth_w_equals_1(self):
         point = Tuple.point(4, -4, 3)
         self.assertEqual(point == Tuple(4, -4, 3, 1), True)
 
+    # vector() creates tuples with w=0
     def test_vector_creates_tuple_wth_w_equals_0(self):
         vector = Tuple.vector(4, -4, 3)
         self.assertEqual(vector == Tuple(4, -4, 3, 0), True)

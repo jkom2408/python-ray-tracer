9ea09db43b065c5852f484395512d58a0f170448
J <jkom2408@gmail.com>
2020-11-10 19:36:42 -0800

    Add wind direction in Z axis for fun exercise

--- src/fun_exercise.py
+++ src/fun_exercise.py
@@ -7,12 +7,12 @@ class FunExercise:
     @staticmethod
     def run():
         p = Projectile(Tuple.point(0, 1, 0), Tuple.vector(1, 1, 0).normalize())
-        e = Environment(Tuple.vector(0, -0.1, 0), Tuple.vector(-0.01, 0, 0))
+        e = Environment(Tuple.vector(0, -0.1, 0), Tuple.vector(-0.01, 0, 0.1))
         r = None
         while r is None or r > 0:
             p = FunExercise.tick(e, p)
             r = p.position.y
-            print(f'x: {p.position.x}, y: {p.position.y}')
+            print(f'x: {p.position.x}, y: {p.position.y}, z: {p.position.z}')
 
     @staticmethod
     def tick(env, proj):

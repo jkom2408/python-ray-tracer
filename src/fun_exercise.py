c32cb3372591049b3963860d553d91f69592d9a8
J <jkom2408@gmail.com>
2020-11-11 14:43:20 -0800

    Fix imports for fun exercise

--- src/fun_exercise.py
+++ src/fun_exercise.py
@@ -1,14 +1,17 @@
+""" A fun exercise using vectors and points """
+
 from projectile import Projectile
 from environment import Environment
-from tuple.tuple import Tuple
+from rtmath.point import Point
+from rtmath.vector import Vector
 
 
 class FunExercise:
 
     @staticmethod
     def run():
-        p = Projectile(Tuple.point(0, 1, 0), Tuple.vector(1, 1, 0).normalize())
-        e = Environment(Tuple.vector(0, -0.1, 0), Tuple.vector(-0.01, 0, 0.1))
+        p = Projectile(Point(0, 1, 0), Vector(1, 1, 0).normalize())
+        e = Environment(Vector(0, -0.1, 0), Vector(-0.01, 0, 0.1))
         r = None
         while r is None or r > 0:
             p = FunExercise.tick(e, p)

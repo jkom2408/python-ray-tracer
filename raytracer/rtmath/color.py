fcb0b149745b817624b02fcb23303e3bdf164410
J <jkom2408@gmail.com>
2020-11-12 01:28:22 -0800

    Implement rt math for colors

--- raytracer/rtmath/color.py
+++ raytracer/rtmath/color.py
@@ -1,3 +1,4 @@
+from numbers import Number
 from .tuple import Tuple
 
 
@@ -5,6 +6,13 @@ class Color(Tuple):
 
     def __init__(self, r, g, b):
         super().__init__(r, g, b, 1)
+
+    @classmethod
+    def new(cls, tuple):
+        return cls(
+            tuple.x,
+            tuple.y,
+            tuple.z)
 
     def r(self):
         return self.x
@@ -17,3 +25,27 @@ class Color(Tuple):
 
     def a(self):
         return self.w
+
+    def __add__(self, other):
+        res = super().__add__(other)
+        return Color.new(res)
+
+    def __sub__(self, other):
+        res = super().__sub__(other)
+        return Color.new(res)
+
+    def __mul__(self, other):
+        if isinstance(other, Number):
+            res = super().__mul__(other)
+            return Color.new(res)
+        elif type(other) is Color:
+            return Color.hadamard_product(self, other)
+        else:
+            raise TypeError
+
+    @staticmethod
+    def hadamard_product(c1, c2):
+        return Color(
+                c1.x * c2.x,
+                c1.y * c2.y,
+                c1.z * c2.z)
--- raytracer/test/tuple/test_color.py
+++ raytracer/test/tuple/test_color.py
@@ -17,6 +17,21 @@ class TestColor(unittest.TestCase):
         self.assertEqual(c.g() == 0.4, True)
         self.assertEqual(c.b() == 1.7, True)
 
+    def test_add_color(self):
+        c1 = Color(0.9, 0.6, 0.75)
+        c2 = Color(0.7, 0.1, 0.25)
+        self.assertEqual(c1 + c2 == Color(1.6, 0.7, 1.0), True)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_subtract_color(self):
+        c1 = Color(0.9, 0.6, 0.75)
+        c2 = Color(0.7, 0.1, 0.25)
+        self.assertEqual(c1 - c2 == Color(0.2, 0.5, 0.5), True)
+
+    def test_multiply_color_by_scalar(self):
+        c = Color(0.2, 0.3, 0.4)
+        self.assertEqual(c * 2 == Color(0.4, 0.6, 0.8), True)
+
+    def test_multiply_color_by_color(self):
+        c1 = Color(1, 0.2, 0.4)
+        c2 = Color(0.9, 1, 0.1)
+        self.assertEqual(c1 * c2 == Color(0.9, 0.2, 0.04), True)

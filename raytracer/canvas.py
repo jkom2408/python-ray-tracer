08f123edd7f9d7a25f5cc2de9dbbeb61b36a54b4
J <jkom2408@gmail.com>
2020-11-13 05:10:28 -0800

    Add ability to output pixel data in PPM format

--- raytracer/canvas.py
+++ raytracer/canvas.py
@@ -1,3 +1,4 @@
+import math
 from raytracer.rtmath.color import Color
 
 
@@ -7,24 +8,34 @@ class Canvas:
         self.width = width
         self.height = height
         self.canvas = [[
-            [0 for c in range(4)]
+            [0 for c in range(3)]
             for w in range(width)]
             for h in range(height)]
 
     def write_pixel(self, x, y, color):
-        self.canvas[y - 1][x - 1][0] = color.r()
-        self.canvas[y - 1][x - 1][1] = color.g()
-        self.canvas[y - 1][x - 1][2] = color.b()
-        self.canvas[y - 1][x - 1][3] = color.a()
+        self.canvas[y][x][0] = color.r()
+        self.canvas[y][x][1] = color.g()
+        self.canvas[y][x][2] = color.b()
+
+    @staticmethod
+    def to_byte(f):
+        if f > 1:
+            return 255
+        elif f < 0:
+            return 0
+        else:
+            return int(math.ceil(255 * f))
 
     def pixel_at(self, x, y):
         return Color(
-            self.canvas[y - 1][x - 1][0],
-            self.canvas[y - 1][x - 1][1],
-            self.canvas[y - 1][x - 1][2])
+           self.canvas[y][x][0],
+           self.canvas[y][x][1],
+           self.canvas[y][x][2])
 
     def sum(self):
         return sum(sum(sum(c) for c in w) for w in self.canvas)
 
     def to_ppm(self):
-        return f'P3\n{self.width} {self.height}\n255'
+        header = f'P3\n{self.width} {self.height}\n255'
+        body = '\n'.join(' '.join(' '.join(map(str, map(Canvas.to_byte, c))) for c in h) for h in self.canvas)
+        return header + '\n' + body

6bb3f286179b55bf50222bfb33fff9735cd1be21
J <jkom2408@gmail.com>
2020-12-05 18:34:58 -0800

    Add implementation for matrix tuple multiplication

--- raytracer/rtmath/matrix.py
+++ raytracer/rtmath/matrix.py
@@ -1,3 +1,6 @@
+from raytracer.rtmath.tuple import Tuple
+
+
 class Matrix:
     def __init__(self, m):
         self.m = m
@@ -30,21 +33,39 @@ class Matrix:
         )
 
     def __mul__(self, other):
+        if isinstance(other, Matrix):
+            return self.__matrix_multiply(self, other)
+        elif isinstance(other, Tuple):
+            return self.__tuplemultiply(self, other)
+        else:
+            raise TypeError
+
+    @staticmethod
+    def __matrix_multiply(a, b):
         # Number of columns on A must equal number of rows on B
-        if len(self[0]) != len(other):
+        if len(a[0]) != len(b):
             raise ValueError
 
         res = Matrix.fromdim(4, 4)
         for r in range(4):
             for c in range(4):
                 res[r][c] = (
-                    self[r][0] * other[0][c] +
-                    self[r][1] * other[1][c] +
-                    self[r][2] * other[2][c] +
-                    self[r][3] * other[3][c]
+                    a[r][0] * b[0][c] +
+                    a[r][1] * b[1][c] +
+                    a[r][2] * b[2][c] +
+                    a[r][3] * b[3][c]
                 )
 
         return res
+
+    @staticmethod
+    def __tuplemultiply(a, b):
+        return Tuple(
+            a[0][0] * b.x + a[0][1] * b.y + a[0][2] * b.z + a[0][3] * b.w,
+            a[1][0] * b.x + a[1][1] * b.y + a[1][2] * b.z + a[1][3] * b.w,
+            a[2][0] * b.x + a[2][1] * b.y + a[2][2] * b.z + a[2][3] * b.w,
+            a[3][0] * b.x + a[3][1] * b.y + a[3][2] * b.z + a[3][3] * b.w
+        )
 
     def __len__(self):
         return len(self.m)

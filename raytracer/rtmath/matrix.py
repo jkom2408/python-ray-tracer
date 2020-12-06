f2089606543e3df4bd6469733307cf039a79ec80
J <jkom2408@gmail.com>
2020-12-05 17:50:04 -0800

    Add matrix implementation with a few basic functions

--- raytracer/rtmath/matrix.py
+++ raytracer/rtmath/matrix.py
@@ -2,5 +2,54 @@ class Matrix:
     def __init__(self, m):
         self.m = m
 
+    @classmethod
+    def fromdim(cls, rows, columns):
+        return cls([[0 for c in range(columns)] for r in range(rows)])
+
     def __getitem__(self, i):
         return self.m[i]
+
+    def __eq__(self, m):
+        return (
+            self.float_eq(self[0][0], m[0][0]) and
+            self.float_eq(self[0][1], m[0][1]) and
+            self.float_eq(self[0][2], m[0][2]) and
+            self.float_eq(self[0][3], m[0][3]) and
+            self.float_eq(self[1][0], m[1][0]) and
+            self.float_eq(self[1][1], m[1][1]) and
+            self.float_eq(self[1][2], m[1][2]) and
+            self.float_eq(self[1][3], m[1][3]) and
+            self.float_eq(self[2][0], m[2][0]) and
+            self.float_eq(self[2][1], m[2][1]) and
+            self.float_eq(self[2][2], m[2][2]) and
+            self.float_eq(self[2][3], m[2][3]) and
+            self.float_eq(self[3][0], m[3][0]) and
+            self.float_eq(self[3][1], m[3][1]) and
+            self.float_eq(self[3][2], m[3][2]) and
+            self.float_eq(self[3][3], m[3][3])
+        )
+
+    def __mul__(self, other):
+        # Number of columns on A must equal number of rows on B
+        if len(self[0]) != len(other):
+            raise ValueError
+
+        res = Matrix.fromdim(4, 4)
+        for r in range(4):
+            for c in range(4):
+                res[r][c] = (
+                    self[r][0] * other[0][c] +
+                    self[r][1] * other[1][c] +
+                    self[r][2] * other[2][c] +
+                    self[r][3] * other[3][c]
+                )
+
+        return res
+
+    def __len__(self):
+        return len(self.m)
+
+    @staticmethod
+    def float_eq(a, b):
+        EPSILON = 0.00001
+        return abs(a - b) < EPSILON
--- test/rtmath/test_matrix.py
+++ test/rtmath/test_matrix.py
@@ -1,8 +1,31 @@
 import unittest
 from raytracer.rtmath.matrix import Matrix
+from raytracer.rtmath.tuple import Tuple
 
 
 class TestMatrix(unittest.TestCase):
+
+    def test_create_matrix_2x2(self):
+        m = Matrix(
+            [
+                [-3, 5],
+                [1, -2],
+            ])
+        self.assertTrue(m[0][0] == -3)
+        self.assertTrue(m[0][1] == 5)
+        self.assertTrue(m[1][0] == 1)
+        self.assertTrue(m[1][1] == -2)
+
+    def test_create_matrix_3x3(self):
+        m = Matrix(
+            [
+                [-3, 5, 0],
+                [1, -2, -7],
+                [0, 1, 1],
+            ])
+        self.assertTrue(m[0][0] == -3)
+        self.assertTrue(m[1][1] == -2)
+        self.assertTrue(m[2][2] == 1)
 
     def test_create_matrix_4x4(self):
         m = Matrix(
@@ -20,24 +43,62 @@ class TestMatrix(unittest.TestCase):
         self.assertTrue(m[3][0] == 13.5)
         self.assertTrue(m[3][2] == 15.5)
 
-    def test_create_matrix_3x3(self):
-        m = Matrix(
-            [
-                [-3, 5, 0],
-                [1, -2, -7],
-                [0, 1, 1],
+    def test_matrix_equality(self):
+        a = Matrix([
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 8, 7, 6],
+                [5, 4, 3, 2]
             ])
-        self.assertTrue(m[0][0] == -3)
-        self.assertTrue(m[1][1] == -2)
-        self.assertTrue(m[2][2] == 1)
+        b = ([
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 8, 7, 6],
+                [5, 4, 3, 2]
+            ])
+        self.assertTrue(a == b)
 
-    def test_create_matrix_2x2(self):
-        m = Matrix(
-            [
-                [-3, 5],
-                [1, -2],
+    def test_matrix_inequality(self):
+        a = Matrix([
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 8, 7, 6],
+                [5, 4, 3, 2]
             ])
-        self.assertTrue(m[0][0] == -3)
-        self.assertTrue(m[0][1] == 5)
-        self.assertTrue(m[1][0] == 1)
-        self.assertTrue(m[1][1] == -2)
+        b = ([
+                [2, 3, 4, 5],
+                [6, 7, 8, 9],
+                [8, 7, 6, 5],
+                [4, 3, 2, 1]
+            ])
+        self.assertTrue(a != b)
+
+    def test_matrix_4x4_multiplication(self):
+        a = Matrix([
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 8, 7, 6],
+                [5, 4, 3, 2]
+            ])
+        b = Matrix([
+                [-2, 1, 2, 3],
+                [3, 2, 1, -1],
+                [4, 3, 6, 5],
+                [1, 2, 7, 8]
+            ])
+        self.assertTrue(a * b == Matrix([
+            [20, 22, 50, 48],
+            [44, 54, 114, 108],
+            [40, 58, 110, 102],
+            [16, 26, 46, 42]
+        ]))
+
+    def test_matrix_tuple_multiplication(self):
+        a = Matrix([
+            [1, 2, 3, 4],
+            [2, 4, 4, 2],
+            [8, 6, 4, 1],
+            [0, 0, 0, 1]
+        ])
+        b = Tuple(1, 2, 3, 1)
+        self.assertTrue(a * b == Tuple(18, 24, 33, 1))

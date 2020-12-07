5ef5a38cbc3c3815891ab8678f5dc67887b6f674
J <jkom2408@gmail.com>
2020-12-06 16:40:37 -0800

    Update a few tests to use assertEqual instead of assertTrue

--- test/rtmath/test_matrix.py
+++ test/rtmath/test_matrix.py
@@ -11,10 +11,10 @@ class TestMatrix(unittest.TestCase):
                 [-3, 5],
                 [1, -2],
             ])
-        self.assertTrue(m[0][0] == -3)
-        self.assertTrue(m[0][1] == 5)
-        self.assertTrue(m[1][0] == 1)
-        self.assertTrue(m[1][1] == -2)
+        self.assertEqual(m[0][0], -3)
+        self.assertEqual(m[0][1], 5)
+        self.assertEqual(m[1][0], 1)
+        self.assertEqual(m[1][1], -2)
 
     def test_create_matrix_3x3(self):
         m = Matrix(
@@ -23,9 +23,9 @@ class TestMatrix(unittest.TestCase):
                 [1, -2, -7],
                 [0, 1, 1],
             ])
-        self.assertTrue(m[0][0] == -3)
-        self.assertTrue(m[1][1] == -2)
-        self.assertTrue(m[2][2] == 1)
+        self.assertEqual(m[0][0], -3)
+        self.assertEqual(m[1][1], -2)
+        self.assertEqual(m[2][2], 1)
 
     def test_create_matrix_4x4(self):
         m = Matrix(
@@ -35,13 +35,13 @@ class TestMatrix(unittest.TestCase):
                 [9, 10, 11, 12],
                 [13.5, 14.5, 15.5, 16.5]
             ])
-        self.assertTrue(m[0][0] == 1)
-        self.assertTrue(m[0][3] == 4)
-        self.assertTrue(m[1][0] == 5.5)
-        self.assertTrue(m[1][2] == 7.5)
-        self.assertTrue(m[2][2] == 11)
-        self.assertTrue(m[3][0] == 13.5)
-        self.assertTrue(m[3][2] == 15.5)
+        self.assertEqual(m[0][0], 1)
+        self.assertEqual(m[0][3], 4)
+        self.assertEqual(m[1][0], 5.5)
+        self.assertEqual(m[1][2], 7.5)
+        self.assertEqual(m[2][2], 11)
+        self.assertEqual(m[3][0], 13.5)
+        self.assertEqual(m[3][2], 15.5)
 
     def test_matrix_equality(self):
         a = Matrix([
@@ -56,7 +56,7 @@ class TestMatrix(unittest.TestCase):
                 [9, 8, 7, 6],
                 [5, 4, 3, 2]
             ])
-        self.assertTrue(a == b)
+        self.assertEqual(a, b)
 
     def test_matrix_inequality(self):
         a = Matrix([
@@ -86,7 +86,7 @@ class TestMatrix(unittest.TestCase):
                 [4, 3, 6, 5],
                 [1, 2, 7, 8]
             ])
-        self.assertTrue(a * b == Matrix([
+        self.assertEqual(a * b, Matrix([
             [20, 22, 50, 48],
             [44, 54, 114, 108],
             [40, 58, 110, 102],
@@ -101,7 +101,7 @@ class TestMatrix(unittest.TestCase):
             [0, 0, 0, 1]
         ])
         b = Tuple(1, 2, 3, 1)
-        self.assertTrue(a * b == Tuple(18, 24, 33, 1))
+        self.assertEqual(a * b, Tuple(18, 24, 33, 1))
 
     def test_identity_matrix(self):
         a = Matrix([
@@ -110,7 +110,7 @@ class TestMatrix(unittest.TestCase):
             [2, 4, 8, 16],
             [4, 8, 16, 32]
         ])
-        self.assertTrue(Matrix.identity() * a == a)
+        self.assertEqual(Matrix.identity() * a, a)
 
     def test_transpose(self):
         a = Matrix([
@@ -119,7 +119,7 @@ class TestMatrix(unittest.TestCase):
             [1, 8, 5, 3],
             [0, 0, 5, 8]
         ])
-        self.assertTrue(Matrix.transpose(a) == Matrix([
+        self.assertEqual(Matrix.transpose(a), Matrix([
             [0, 9, 1, 0],
             [9, 8, 8, 0],
             [3, 0, 5, 5],
